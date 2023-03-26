eb1fa02a011fac5cac70fd480e65fa80e55a759f
Chris Koop <Chris.Koop@oit.edu>
2023-03-26 09:07:22 +0900

    added existing code

---
+++ src/Config.h
@@ -0,0 +1,107 @@
+// Singleton Config class
+#include <fstream>
+#include <string>
+#include <iostream>
+using std::string;
+using std::ifstream;
+using std::map;
+using std::getline;
+using std::cout;
+
+class Config
+{
+    private:
+        static Config* instance;
+        string filename;
+
+        // Actual config parameters
+        int windowWidth;
+        int windowHeight;
+        int circleSize;
+        int circleNum;
+
+        Config(){}
+        Config(const Config&) = delete;
+        Config& operator = (const Config&) = delete;
+
+    public:
+        static Config* getInstance()
+        {
+            return instance;
+        }
+
+        static Config* getInstance(int argc, char* argv[])
+        {
+            if (!instance)
+            {
+                instance = new Config();
+                instance -> parseCommandLineArgs(argc, argv);
+                instance -> readFile();
+            }
+            return instance;
+        }
+
+        int getWindowWidth()
+        {
+            return windowWidth;
+        }
+
+        int getWindowHeight()
+        {
+            return windowHeight;
+        }
+
+        int getCircleSize()
+        {
+            return circleSize;
+        }
+
+        int getCircleNum()
+        {
+            return circleNum;
+        }
+
+    private:
+        // Parse command line arguments to get config file
+        void parseCommandLineArgs(int argc, char* argv[])
+        {
+            for (int i=0; i<argc; i++)
+            {
+                if (string(argv[i]) == "-config")
+                {
+                    filename = argv[i+1];
+                }
+            }
+        }
+
+        // Read config file
+        void readFile()
+        {
+            ifstream file(filename);
+            string line;
+            while (getline(file, line))
+            {
+                size_t posi = line.find("=");
+                if (posi != -1)
+                {
+                    string key = line.substr(0, posi);
+                    string value = line.substr(posi+1);
+                    if (key == "windowWidth") {
+                        windowWidth = atoi(value.c_str());
+                    } else if (key == "windowHeight") {
+                        windowHeight = atoi(value.c_str());
+                    } else if (key == "circleSize") {
+                        circleSize = atoi(value.c_str());
+                    } else if (key == "circleNum") {
+                        circleNum = atoi(value.c_str());
+                    }
+                }
+            }
+            cout << "windowWidth: " << windowWidth << std::endl;
+            cout << "windowHeight: " << windowHeight << std::endl;
+            cout << "circleNum: " << circleNum << std::endl;
+            cout << "circleSize: " << circleSize << std::endl;
+        }
+};
+
+Config* Config::instance = nullptr;
