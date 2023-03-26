96b71383098b91f7637341ba61b6fcfc7efcd444
Chris Koop <Chris.Koop@oit.edu>
2023-03-26 09:32:55 +0900

    Fixed to used Meyer's

--- src/Config.h
+++ src/Config.h
@@ -10,100 +10,94 @@ using std::cout;
 
 class Config
 {
-    private:
-        static Config* instance;
-        string filename;
+private:
+    string filename;
+    // Actual config parameters
+    uint windowWidth;
+    uint windowHeight;
+    float circleSize;
+    uint circleNum;
 
-        // Actual config parameters
-        uint windowWidth;
-        uint windowHeight;
-        float circleSize;
-        uint circleNum;
+    Config(){}
+    Config(const Config&) = delete;
+    Config& operator = (const Config&) = delete;
 
-        Config(){}
-        Config(const Config&) = delete;
-        Config& operator = (const Config&) = delete;
+public:
+    static Config& getInstance()
+    {
+        static Config instance;
+        return instance;
+    }
 
-    public:
-        static Config* getInstance()
+    static Config& getInstance(int argc, char* argv[])
+    {
+        Config& instance = getInstance();
+        instance.parseCommandLineArgs(argc, argv);
+        instance.readFile();
+        return instance;
+    }
+
+    uint getWindowWidth()
+    {
+        return windowWidth;
+    }
+
+    uint getWindowHeight()
+    {
+        return windowHeight;
+    }
+
+    float getCircleSize()
+    {
+        return circleSize;
+    }
+
+    uint getCircleNum()
+    {
+        return circleNum;
+    }
+
+private:
+    // Parse command line arguments to get config file
+    void parseCommandLineArgs(int argc, char* argv[])
+    {
+        for (int i=0; i<argc; i++)
         {
-            return instance;
-        }
-
-        static Config* getInstance(int argc, char* argv[])
-        {
-            if (!instance)
+            if (string(argv[i]) == "-config")
             {
-                instance = new Config();
-                instance -> parseCommandLineArgs(argc, argv);
-                instance -> readFile();
+                filename = argv[i+1];
             }
-            return instance;
         }
+    }
 
-        uint getWindowWidth()
+    // Read config file
+    void readFile()
+    {
+        ifstream file(filename);
+        string line;
+        while (getline(file, line))
         {
-            return windowWidth;
-        }
-
-        uint getWindowHeight()
-        {
-            return windowHeight;
-        }
-
-        float getCircleSize()
-        {
-            return circleSize;
-        }
-
-        uint getCircleNum()
-        {
-            return circleNum;
-        }
-
-    private:
-        // Parse command line arguments to get config file
-        void parseCommandLineArgs(int argc, char* argv[])
-        {
-            for (int i=0; i<argc; i++)
+            size_t posi = line.find("=");
+            if (posi != -1)
             {
-                if (string(argv[i]) == "-config")
-                {
-                    filename = argv[i+1];
+                string key = line.substr(0, posi);
+                string value = line.substr(posi+1);
+                if (key == "windowWidth") {
+                    windowWidth = atoi(value.c_str());
+                } else if (key == "windowHeight") {
+                    windowHeight = atoi(value.c_str());
+                } else if (key == "circleSize") {
+                    circleSize = atoi(value.c_str());
+                } else if (key == "circleNum") {
+                    circleNum = atoi(value.c_str());
                 }
             }
         }
-
-        // Read config file
-        void readFile()
-        {
-            ifstream file(filename);
-            string line;
-            while (getline(file, line))
-            {
-                size_t posi = line.find("=");
-                if (posi != -1)
-                {
-                    string key = line.substr(0, posi);
-                    string value = line.substr(posi+1);
-                    if (key == "windowWidth") {
-                        windowWidth = atoi(value.c_str());
-                    } else if (key == "windowHeight") {
-                        windowHeight = atoi(value.c_str());
-                    } else if (key == "circleSize") {
-                        circleSize = atoi(value.c_str());
-                    } else if (key == "circleNum") {
-                        circleNum = atoi(value.c_str());
-                    }
-                }
-            }
-            cout << "windowWidth: " << windowWidth << std::endl;
-            cout << "windowHeight: " << windowHeight << std::endl;
-            cout << "circleNum: " << circleNum << std::endl;
-            cout << "circleSize: " << circleSize << std::endl;
-        }
+        cout << "windowWidth: " << windowWidth << std::endl;
+        cout << "windowHeight: " << windowHeight << std::endl;
+        cout << "circleNum: " << circleNum << std::endl;
+        cout << "circleSize: " << circleSize << std::endl;
+    }
 };
-
-Config* Config::instance = nullptr;
 
 // end of config.h
--- src/lab1.cpp
+++ src/lab1.cpp
@@ -28,48 +28,47 @@ void processEvents(sf::RenderWindow & window, std::vector<sf::CircleShape> & sha
 
 void update(sf::Time dt, std::vector<sf::CircleShape> & shapes) {
   float s = dt.asSeconds();
-  Config* config = Config::getInstance();
+  Config& config = Config::getInstance();
   for (auto & shape : shapes) {
     shape.move(20.0f * s, 20.0f * s);
-    if (shape.getPosition().x > config->getWindowWidth())
+    if (shape.getPosition().x > config.getWindowWidth())
     {
       shape.setPosition(0, shape.getPosition().y);
     }
-    else if ( shape.getPosition().x < 0)
+    else if (shape.getPosition().x < 0)
     {
-      shape.setPosition(config->getWindowWidth(), shape.getPosition().y);
+      shape.setPosition(config.getWindowWidth(), shape.getPosition().y);
     }
 
-
-    if (shape.getPosition().y > config->getWindowHeight())
+    if (shape.getPosition().y > config.getWindowHeight())
     {
       shape.setPosition(shape.getPosition().x, 0);
     }
     else if (shape.getPosition().y < 0)
     {
-      shape.setPosition(shape.getPosition().x, config->getWindowHeight());
+      shape.setPosition(shape.getPosition().x, config.getWindowHeight());
     }
   }
 }
 
 int main(int argc, char *argv[]) {
   // get config values
-  Config* config = Config::getInstance(argc, argv);
+  Config& config = Config::getInstance(argc, argv);
 
-  sf::RenderWindow window{sf::VideoMode{config->getWindowWidth(), config->getWindowHeight()}, "Lab1"};
+  sf::RenderWindow window{sf::VideoMode{config.getWindowWidth(), config.getWindowHeight()}, "Lab1"};
 
   std::vector<sf::CircleShape> shapes{};
 
-  for (int i{}; i < config->getCircleNum(); ++i) {
-    sf::CircleShape shape{config->getCircleSize(), 100};
+  for (int i{}; i < config.getCircleNum(); ++i) {
+    sf::CircleShape shape{config.getCircleSize(), 100};
     shape.setFillColor(sf::Color::Blue);
     shape.setOutlineColor(sf::Color::White);
-    shape.setPosition(rand(22.0f, config->getWindowWidth() - 22.0f), rand(22.0f, config->getWindowHeight() - 22.0f));
+    shape.setPosition(rand(22.0f, config.getWindowWidth() - 22.0f), rand(22.0f, config.getWindowHeight() - 22.0f));
     shapes.push_back(shape);
   }
 
   // for info on game loops:
-  //   https://subscription.packtpub.com/book/game+development/9781849696845/1/ch01lvl1sec11/game-loops-and-frames
+  // https://subscription.packtpub.com/book/game+development/9781849696845/1/ch01lvl1sec11/game-loops-and-frames
   sf::Clock clock;
 
   sf::Time t{sf::Time::Zero}; // time
